2f95425d633206128a3e54771623a5afd0f4b051
Nehal Kanetkar <nkanetkar@pelmorex.com>
2016-03-09 22:31:58 -0500

    Refactor js

--- animate.js
+++ animate.js
@@ -1,5 +1,6 @@
-function HomeControl(controlDiv, map) {
+var HomeControl = function(controlDiv, map) {
   	controlDiv.style.padding = '10px';
+
   	var controlUI = document.createElement('div');
   	controlUI.style.backgroundColor = 'white';
   	controlUI.style.borderRadius = '2px';
@@ -7,6 +8,7 @@ function HomeControl(controlDiv, map) {
 	controlUI.style.textAlign = 'center';
 	controlUI.title = 'Change Opacity Button';
 	controlDiv.appendChild(controlUI);
+
 	var controlText = document.createElement('div');
 	controlText.style.fontFamily='Helvetica-neue,sans-serif';
 	controlText.style.fontSize='12px';
@@ -20,9 +22,9 @@ function HomeControl(controlDiv, map) {
   	google.maps.event.addDomListener(controlUI, 'click', function() {
     	changeOpacity();
   	});
-};
+},
 
-var changeOpacity = function() {
+changeOpacity = function() {
 	var wrapper = document.getElementById("wrapper"),
 		mapOpacity = wrapper.style.opacity;
 
@@ -31,33 +33,32 @@ var changeOpacity = function() {
 	} else {
 		wrapper.style.opacity = 1.0;
 	}
-};
+},
 
-var initialize = function () {
-	var mapCanvas = document.getElementById('map');
-	var mapOptions = {
+initialize = function () {
+	var mapCanvas = document.getElementById('map'),
+		mapOptions = {
 		center: new google.maps.LatLng(44.256458, -76.744456),
 		zoom: 5,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		styles:styles
-	}
+	},
 	
-	var map = new google.maps.Map(mapCanvas,mapOptions);
-	var centerControlDiv = document.createElement('div');
+	map = new google.maps.Map(mapCanvas,mapOptions),
+	centerControlDiv = document.createElement('div');
 	map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
 	// Create a DIV to hold the control and call HomeControl()
   	var homeControlDiv = document.createElement('div');
-  	var homeControl = new HomeControl(homeControlDiv, map);
+  		homeControl = new HomeControl(homeControlDiv, map),
   	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
 
 	var flightPath = {
 		path: google.maps.SymbolPath.CIRCLE,
 		scale:1,
 		strokeColor: '#FFF'
-	};
-
-	var flightPathIcon = [{icon:flightPath,offset:'100%'}]
+	},
+		flightPathIcon = [{icon:flightPath,offset:'100%'}];
 
 	var poe610 = new google.maps.Polyline({
 		path: POE610Coords,
@@ -65,561 +66,561 @@ var initialize = function () {
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe532 = new google.maps.Polyline({
+	poe532 = new google.maps.Polyline({
 		path: POE532Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe572 = new google.maps.Polyline({
+	poe572 = new google.maps.Polyline({
 		path: POE572Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe246 = new google.maps.Polyline({
+	poe246 = new google.maps.Polyline({
 		path: POE246Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe684 = new google.maps.Polyline({
+	poe684 = new google.maps.Polyline({
 		path: POE684Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe406 = new google.maps.Polyline({
+	poe406 = new google.maps.Polyline({
 		path: POE406Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe938 = new google.maps.Polyline({
+	poe938 = new google.maps.Polyline({
 		path: POE938Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe250 = new google.maps.Polyline({
+	poe250 = new google.maps.Polyline({
 		path: POE250Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe122 = new google.maps.Polyline({
+	poe122 = new google.maps.Polyline({
 		path: POE122Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe384 = new google.maps.Polyline({
+	poe384 = new google.maps.Polyline({
 		path: POE384Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe408 = new google.maps.Polyline({
+	poe408 = new google.maps.Polyline({
 		path: POE408Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe412 = new google.maps.Polyline({
+	poe412 = new google.maps.Polyline({
 		path: POE412Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe124 = new google.maps.Polyline({
+	poe124 = new google.maps.Polyline({
 		path: POE124Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe252 = new google.maps.Polyline({
+	poe252 = new google.maps.Polyline({
 		path: POE252Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe536 = new google.maps.Polyline({
+	poe536 = new google.maps.Polyline({
 		path: POE536Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe418 = new google.maps.Polyline({
+	poe418 = new google.maps.Polyline({
 		path: POE418Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe258 = new google.maps.Polyline({
+	poe258 = new google.maps.Polyline({
 		path: POE258Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe686 = new google.maps.Polyline({
+	poe686 = new google.maps.Polyline({
 		path: POE686Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe662 = new google.maps.Polyline({
+	poe662 = new google.maps.Polyline({
 		path: POE662Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe574 = new google.maps.Polyline({
+	poe574 = new google.maps.Polyline({
 		path: POE574Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe386 = new google.maps.Polyline({
+	poe386 = new google.maps.Polyline({
 		path: POE386Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe634 = new google.maps.Polyline({
+	poe634 = new google.maps.Polyline({
 		path: POE634Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe512 = new google.maps.Polyline({
+	poe512 = new google.maps.Polyline({
 		path: POE512Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe940 = new google.maps.Polyline({
+	poe940 = new google.maps.Polyline({
 		path: POE940Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe132 = new google.maps.Polyline({
+	poe132 = new google.maps.Polyline({
 		path: POE132Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe724 = new google.maps.Polyline({
+	poe724 = new google.maps.Polyline({
 		path: POE724Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe260 = new google.maps.Polyline({
+	poe260 = new google.maps.Polyline({
 		path: POE260Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe354 = new google.maps.Polyline({
+	poe354 = new google.maps.Polyline({
 		path: POE354Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe420 = new google.maps.Polyline({
+	poe420 = new google.maps.Polyline({
 		path: POE420Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe630 = new google.maps.Polyline({
+	poe630 = new google.maps.Polyline({
 		path: POE630Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe136 = new google.maps.Polyline({
+	poe136 = new google.maps.Polyline({
 		path: POE136Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe716 = new google.maps.Polyline({
+	poe716 = new google.maps.Polyline({
 		path: POE716Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe424 = new google.maps.Polyline({
+	poe424 = new google.maps.Polyline({
 		path: POE424Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe264 = new google.maps.Polyline({
+	poe264 = new google.maps.Polyline({
 		path: POE264Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe140 = new google.maps.Polyline({
+	poe140 = new google.maps.Polyline({
 		path: POE140Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe409 = new google.maps.Polyline({
+	poe409 = new google.maps.Polyline({
 		path: POE409Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe125 = new google.maps.Polyline({
+	poe125 = new google.maps.Polyline({
 		path: POE125Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe249 = new google.maps.Polyline({
+	poe249 = new google.maps.Polyline({
 		path: POE249Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe661 = new google.maps.Polyline({
+	poe661 = new google.maps.Polyline({
 		path: POE661Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe413 = new google.maps.Polyline({
+	poe413 = new google.maps.Polyline({
 		path: POE413Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe633 = new google.maps.Polyline({
+	poe633 = new google.maps.Polyline({
 		path: POE633Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe939 = new google.maps.Polyline({
+	poe939 = new google.maps.Polyline({
 		path: POE939Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe715 = new google.maps.Polyline({
+	poe715 = new google.maps.Polyline({
 		path: POE715Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe253 = new google.maps.Polyline({
+	poe253 = new google.maps.Polyline({
 		path: POE253Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe385 = new google.maps.Polyline({
+	poe385 = new google.maps.Polyline({
 		path: POE385Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe535 = new google.maps.Polyline({
+	poe535 = new google.maps.Polyline({
 		path: POE535Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe511 = new google.maps.Polyline({
+	poe511 = new google.maps.Polyline({
 		path: POE511Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe129 = new google.maps.Polyline({
+	poe129 = new google.maps.Polyline({
 		path: POE129Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe685 = new google.maps.Polyline({
+	poe685 = new google.maps.Polyline({
 		path: POE685Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe415 = new google.maps.Polyline({
+	poe415 = new google.maps.Polyline({
 		path: POE415Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe131 = new google.maps.Polyline({
+	poe131 = new google.maps.Polyline({
 		path: POE131Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe723 = new google.maps.Polyline({
+	poe723 = new google.maps.Polyline({
 		path: POE723Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe573 = new google.maps.Polyline({
+	poe573 = new google.maps.Polyline({
 		path: POE573Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe257 = new google.maps.Polyline({
+	poe257 = new google.maps.Polyline({
 		path: POE257Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe135 = new google.maps.Polyline({
+	poe135 = new google.maps.Polyline({
 		path: POE135Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe353 = new google.maps.Polyline({
+	poe353 = new google.maps.Polyline({
 		path: POE353Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe419 = new google.maps.Polyline({
+	poe419 = new google.maps.Polyline({
 		path: POE419Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe635 = new google.maps.Polyline({
+	poe635 = new google.maps.Polyline({
 		path: POE635Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe391 = new google.maps.Polyline({
+	poe391 = new google.maps.Polyline({
 		path: POE391Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe613 = new google.maps.Polyline({
+	poe613 = new google.maps.Polyline({
 		path: POE613Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe259 = new google.maps.Polyline({
+	poe259 = new google.maps.Polyline({
 		path: POE259Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe945 = new google.maps.Polyline({
+	poe945 = new google.maps.Polyline({
 		path: POE945Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe687 = new google.maps.Polyline({
+	poe687 = new google.maps.Polyline({
 		path: POE687Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe421 = new google.maps.Polyline({
+	poe421 = new google.maps.Polyline({
 		path: POE421Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe261 = new google.maps.Polyline({
+	poe261 = new google.maps.Polyline({
 		path: POE261Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe139 = new google.maps.Polyline({
+	poe139 = new google.maps.Polyline({
 		path: POE139Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe575 = new google.maps.Polyline({
+	poe575 = new google.maps.Polyline({
 		path: POE575Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe263 = new google.maps.Polyline({
+	poe263 = new google.maps.Polyline({
 		path: POE263Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe637 = new google.maps.Polyline({
+	poe637 = new google.maps.Polyline({
 		path: POE637Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe539 = new google.maps.Polyline({
+	poe539 = new google.maps.Polyline({
 		path: POE539Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
 		strokeWeight: 0,
 		map:map
-	});
+	}),
 
-	var poe423 = new google.maps.Polyline({
+	poe423 = new google.maps.Polyline({
 		path: POE423Coords,
 		icons: [{icon:flightPath,offset:'100%'}],
 		strokeOpacity: 1.0,
@@ -698,17 +699,18 @@ var initialize = function () {
 	setTimeout(function() {animateFlight(poe637,44.0)},4000);
 	setTimeout(function() {animateFlight(poe539,44.0)},3000);
 	setTimeout(function() {animateFlight(poe423,44.0)},000);
-}
+},
 
-function animateFlight(poe, speed){
-	var count = 0;
+animateFlight = function(poe, speed){
+	var count = 0,
+		icons;
 	window.setInterval(function() {
 	count = (count + 1) //% 200; // Add modulus something to get it to repeat
 
-	var icons = poe.get('icons');
+	icons = poe.get('icons');
 	icons[0].offset = (count / 2) + '%';
 	poe.set('icons', icons);
 	}, 20);
-}
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
